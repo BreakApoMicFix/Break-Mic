1aa4fb73c45e3860b309e370fc4dacac8484081f
BreakApoMicFix <43aondclol@gmail.com>
2025-11-23 02:08:05 +0200

    Delete fix.ps1

--- fix.ps1
+++
@@ -1,8 +0,0 @@
-$p = {
-    while($true){
-        $p = Get-Process audiodg -ErrorAction SilentlyContinue
-        if($p){$p.ProcessorAffinity=[IntPtr]1}
-        Start-Sleep -Seconds 5
-    }
-}
-Start-Process powershell -WindowStyle Hidden -ArgumentList "-NoProfile -Command &$p"
